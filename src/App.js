04fb1d45d1d7af1dc6bdc33fa67ae0d52763f8f0
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-08 15:18:19 +0530

    adding wikipedia api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Acccordian from "./Accordion";
+import Search from "./Search";
 
-function App() {
+const items = [
+  {
+    title: "What is React",
+    content: "React is a frontend js framework",
+  },
+  {
+    title: "Why to use React",
+    content: "So handy and easy to use",
+  },
+  {
+    title: "How to structure react project",
+    content: "By dividing it into different structures",
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Acccordian items={items}></Acccordian> */}
+      <Search></Search>{" "}
     </div>
   );
-}
-
+};
 export default App;
