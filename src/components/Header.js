c9d34f19977a54ad57e89112ad517d45775f7d73
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-09 22:33:54 +0530

    completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Link from './Link';
+
 const Header = () => {
     return (
         <div className = "ui secondary pointing menu">
-            <a className="item" href="/">Accordian</a>
-            <a className="item" href="/list">Search</a>
-            <a className="item" href="/dropdown">Dropdown</a>
-            <a className="item" href="/translate">Translate</a>
+            <Link className="item" href="/">Accordian</Link>
+            <Link className="item" href="/list">Search</Link>
+            <Link className="item" href="/dropdown">Dropdown</Link>
+            <Link className="item" href="/translate">Translate</Link>
         </div>
     )
 }
