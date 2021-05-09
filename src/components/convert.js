415f03ba1151e0a1b561fdaaec4ed9079f89c4e0
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-09 13:45:55 +0530

    adding navigation

--- src/components/convert.js
+++ src/components/convert.js
@@ -1,9 +1,39 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+  const [debounce, setDebounce] = useState(text);
+
   useEffect(() => {
-    console.log("useEffect trigerred");
-  }, [language, text]);
-  return <div></div>;
+    const timerId = setTimeout(() => {
+      setDebounce(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+  useEffect(() => {
+    const translate = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: debounce,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    translate();
+  }, [debounce, language]);
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 export default Convert;
